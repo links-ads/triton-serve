2fd04b200026d9ba5b374ffa7f02101f2c1bf49e
Edoardo Arnaudo <edoardo.arnaudo@linksfoundation.com>
2024-02-07 12:28:02 +0100

    Add service configuration params (#7)

--- alembic/versions/48ee7ea316c5_populate.py
+++ alembic/versions/48ee7ea316c5_populate.py
@@ -1,10 +1,12 @@
 """populate
 
 Revision ID: 48ee7ea316c5
-Revises: 3373b9da9862
+Revises: 915f97395c0d
 Create Date: 2023-12-22 10:58:21.040444
 
 """
+
+import logging
 from collections.abc import Sequence
 
 import sqlalchemy as sa
@@ -15,9 +17,10 @@ from triton_serve.database.model import Device, Machine
 
 # revision identifiers, used by Alembic.
 revision: str = "48ee7ea316c5"
-down_revision: str | None = "3373b9da9862"
+down_revision: str | None = "915f97395c0d"
 branch_labels: str | (Sequence[str] | None) = None
 depends_on: str | (Sequence[str] | None) = None
+log = logging.getLogger("alembic")
 
 
 def upgrade() -> None:
@@ -29,7 +32,11 @@ def upgrade() -> None:
             total_memory=total_mem,
         )
     )
-    gpus = get_gpu_info()
+    try:
+        gpus = get_gpu_info()
+    except Exception as e:
+        log.warning(f"Failed to get GPU info: {e}")
+        gpus = []
     for gpu in gpus:
         op.execute(sa.insert(Device).values(host_id=1, **gpu))
 
