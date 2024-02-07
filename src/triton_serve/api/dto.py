9ba044aa5e19e1ca5405ffa8a24f3487d9b6afcf
Edoardo Arnaudo <edoardo.arnaudo@linksfoundation.com>
2024-02-07 12:28:02 +0100

    Add service configuration params (#7)

--- src/triton_serve/api/dto.py
+++ src/triton_serve/api/dto.py
@@ -7,16 +7,23 @@ class ModelCreateBody(BaseModel):
     version: int
     description: str | None
 
-    def __init__(self, name: str = Form(...), version: int = Form(1), description: str = Form(None)):
+    def __init__(
+        self,
+        name: str = Form(...),
+        version: int = Form(1),
+        description: str = Form(None),
+    ):
         super().__init__(name=name, version=version, description=description)
 
     @field_validator("name")
+    @classmethod
     def validate_name(cls, v):
         if not v:
             raise ValueError("Model name cannot be empty")
         return v
 
     @field_validator("version")
+    @classmethod
     def validate_version(cls, v):
         if v is None:
             raise ValueError("Model version cannot be empty")
@@ -33,25 +40,29 @@ class ServiceCreateModel(BaseModel):
 class ServiceCreateBody(BaseModel):
     name: str
     models: list[ServiceCreateModel]
-    gpu: bool = False
+    gpus: int = 0
     docker_image: str | None = None
+    environment: dict[str, str] | None = None
 
     @field_validator("name")
+    @classmethod
     def validate_name(cls, v):
         if not v:
             raise ValueError("Service name cannot be empty")
         return v
 
+    @field_validator("models")
+    @classmethod
+    def validate_models(cls, v):
+        if not v:
+            raise ValueError("Service must have at least one model")
+        return v
 
-class GPUDevice(BaseModel):
-    uuid: str
-    name: str
-    memory: int
-    index: int
-
-
-class MachineSchema(BaseModel):
-    host_name: str
-    num_cpus: int
-    total_memory: int
-    devices: list[GPUDevice] = []
+    @field_validator("gpus")
+    @classmethod
+    def validate_gpus(cls, v):
+        if v is None:
+            raise ValueError("Number of GPUs cannot be empty")
+        if not isinstance(v, int) or v < 0:
+            raise ValueError("Number of GPUs needs to be a non-negative integer")
+        return v
--- src/triton_serve/api/services/resources.py
+++ src/triton_serve/api/services/resources.py
@@ -5,36 +5,48 @@ import subprocess
 
 import psutil
 
-from triton_serve.api.dto import GPUDevice
+from triton_serve.database.schema import DeviceCreateSchema
 
 
 def get_gpu_info(
     executable: str = "nvidia-smi",
     field_format: str = "csv,noheader,nounits",
-    fields: list[str] = ["index", "uuid", "memory.total", "name"],
-):
-    output = []
-    try:
-        command = f"{executable} --query-gpu={','.join(fields)} --format={field_format}".split(" ")
+    fields: list[str] = None,
+) -> list[DeviceCreateSchema]:
+    """Get the GPU information using nvidia-smi.
 
-        out = subprocess.run(command, capture_output=True, check=True)
-        gpus_info = out.stdout.decode(encoding="utf-8")
-        reader = csv.DictReader(io.StringIO(gpus_info), fieldnames=fields, skipinitialspace=True)
-        gpus = []
-        for line in reader:
-            gpu = GPUDevice(
-                uuid=line["uuid"],
-                name=line["name"],
-                memory=int(line["memory.total"]),
-                index=int(line["index"]),
-            )
-            gpus.append(gpu)
-        return gpus
-    except Exception:
-        return output
+    Args:
+        executable (str, optional): The command to run. Defaults to "nvidia-smi".
+        field_format (str, optional): what to include in the output. Defaults to "csv,noheader,nounits".
+        fields (list[str], optional): which fields to query. Defaults to None.
+
+    Returns:
+        list[DeviceCreateSchema]: list of GPUs of this node.
+    """
+    fields = fields or ["index", "uuid", "memory.total", "name"]
+    command = f"{executable} --query-gpu={','.join(fields)} --format={field_format}".split(" ")
+
+    out = subprocess.run(command, capture_output=True, check=True)
+    gpus_info = out.stdout.decode(encoding="utf-8")
+    reader = csv.DictReader(io.StringIO(gpus_info), fieldnames=fields, skipinitialspace=True)
+    gpus = []
+    for line in reader:
+        gpu = DeviceCreateSchema(
+            uuid=line["uuid"],
+            name=line["name"],
+            memory=int(line["memory.total"]),
+            index=int(line["index"]),
+        )
+        gpus.append(gpu)
+    return gpus
 
 
-def get_machine_info():
+def get_machine_info() -> tuple[str, int, int]:
+    """Get the machine information.
+
+    Returns:
+        tuple[str, int, int]: hostname, number of cpus, total memory.
+    """
     num_cpus = psutil.cpu_count(logical=True)
     mem_info = psutil.virtual_memory()
     total_mem = mem_info.total >> 20
